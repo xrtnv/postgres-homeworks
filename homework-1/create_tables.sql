cfd196830374022131dcb324e838a70288e3d295
xrtnv <xrtnv@xrtnv.com>
2024-02-19 16:51:27 +0300

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    title VARCHAR(255),
+    birth_date DATE,
+    notes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id VARCHAR(255) PRIMARY KEY,
+    company_name VARCHAR(255),
+    contact_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT PRIMARY KEY,
+    customer_id VARCHAR(255),
+    employee_id INT,
+    order_date DATE,
+    ship_city VARCHAR(255),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
